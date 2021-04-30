ffd10200d1123ec47007a58dec9f0dadab958684
tume30938 <tume30938@gmail.com>
2021-04-30 04:08:08 +0000

    modify

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var express = require('express')
-var path = require('path')
-//var handlerServer = require('')
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    redisStore = require('connect-redis')(session)
+
+let redisClient = require('./db/redis')
 
 var app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(session({
+    secret: 'xxx',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 1,
+        httpOnly: true
+    },
+    store: new redisStore({ client: redisClient})
+}))
 
 
 app.get('/', (req, res) => {
@@ -10,5 +27,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log('listen.......')
+    console.log(`listen.......${process.env.PORT}`)
 })
